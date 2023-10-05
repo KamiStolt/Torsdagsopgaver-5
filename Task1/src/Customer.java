23e6b1c46b643f58b40e2308b3bdc25be965d833
KamiStolt <k.stoltenberg@hotmail.com>
2023-10-05 20:33:37 +0200

    Task 1

--- Task1/src/Customer.java
+++ Task1/src/Customer.java
@@ -1,3 +1,32 @@
+
+
 public class Customer {
-    
+   private String firstName;
+   private String lastName;
+   private String userName;
+   private int id;
+
+   /* 1.b Klassen skal have en konstruktør,
+    der tager kundens navn og brugernavn som parametre.
+    Giv klassen en toString() metode, der printer kundens
+    detaljer pænt ud. Gør alle klassens felter private, og tilføj getters().
+*/
+   public Customer(String firstName, String lastName, String userName){
+       this.firstName = firstName;
+       this.lastName = lastName;
+       this.userName = userName;
+   }
+public String toString(){
+    return "navn: "+ getFirstName()+" " + getLastName() +"\nusername: "+ getUserName();
+
 }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+   }
--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Customer> customers = new ArrayList<>();
+        Customer c1 = new Customer("Didrik", "Declan", "Bueskytte");
+        customers.add(c1);
+        System.out.println(customers);
+    }
+
+    public static void printCustomers(ArrayList<Customer> customers) {
+        for (Customer j : customers) {
+            System.out.println(j);
+
+        }
     }
 }
